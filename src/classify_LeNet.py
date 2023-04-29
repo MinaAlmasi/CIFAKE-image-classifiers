71925e1032cbb33757c2bc9ecc7edd126f7b07c9
MinaAlmasi <minaalmasi00@gmail.com>
2023-04-29 20:02:44 +0200

    fix mistake

--- src/classify_LeNet.py
+++ src/classify_LeNet.py
@@ -43,7 +43,7 @@ def cnn_lenet(input_shape:tuple=(32, 32, 3), output_layer_size:int=10):  # adapt
     # first set of layers CONV => RELU => MAXPOOL
     model.add(Conv2D(32, (3,3),
                     padding="same",
-                    input_shape=input_shape, activation=relu))
+                    input_shape=input_shape, activation="relu"))
     model.add(MaxPooling2D(pool_size = (2,2),
                         strides = (2,2)))
 
