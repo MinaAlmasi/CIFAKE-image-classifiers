92b03638bce934f3db4f77fa21b7c2c2eeb31781
MinaAlmasi <minaalmasi00@gmail.com>
2023-05-06 19:46:37 +0200

    make table, add tabulate to reqs

--- src/tables.py
+++ src/tables.py
@@ -1,16 +1,25 @@
-import pickle
+# utils 
 import pathlib 
+import re 
+
+# data wrangling 
 import pandas as pd 
 
-def create_data_from_metric_txt(path):
+# make table
+from tabulate import tabulate
+
+def create_data_from_metrics_txt(filepath):
     '''
     Create a dataframe from a text file containing the classification report from sklearn.metrics.classification_report
 
     Args:
-        - path: path to text file
+        - filepath: path to text file
+
+    Returns: 
+        - data: dataframe containing the classification report 
     '''
 
-    data = pd.read_csv(path)
+    data = pd.read_csv(filepath)
 
     # replace macro avg and weighted avg with macro_avg and weighted_avg
     data.iloc[:,0]= data.iloc[:,0].str.replace(r'(macro|weighted)\savg', r'\1_avg', regex=True)
@@ -31,19 +40,65 @@ def create_data_from_metric_txt(path):
     # set precision and recall to None for the accuracy row
     data.loc[is_accuracy, ['precision', 'recall']] = None
 
-    return data 
+    return data
+
+def create_metrics_dataframes(resultspath):
+    '''
+    Loads all history objects from a given path and returns them in a dictionary.
+
+    Args:
+        - resultspath: path to directory containing txt files with metrics from scikit-learn's classification report
+    
+    Returns: 
+        - metrics_dfs: dictionary containing all txt files in path 
+    '''
+
+    # define empty dictionary where dataframes will be saved
+    metrics_dfs = {}
+
+    for file in resultspath.iterdir(): 
+        if "metrics" in file.name: # only work on all files which have "metrics in their name"
+            # create dataframe from txt file 
+            metrics_data = create_data_from_metrics_txt(resultspath/file.name)
+            # define metrics name with regex (e.g., "REAL_LeNet_18e.txt" -> "REAL_LeNet")
+            metrics_name = re.sub("_metrics_\d+e.txt", "", file.name)
+            # add to metrics_dfs dict ! 
+            metrics_dfs[metrics_name] = metrics_data
+
+    return metrics_dfs
+
+def create_table(data_dict): 
+    # get header for table   
+    header_labels = data_dict["REAL_VGG16"]["class"].tolist()
+
+    print(header_labels)
+
+    table = tabulate(
+        [["REAL NN"] + data_dict["REAL_NN"]["f1-score"].tolist(),   
+        ["FAKE NN"] + data_dict["FAKE_NN"]["f1-score"].tolist(),   
+        ["REAL LeNet"] + data_dict["REAL_LeNet"]["f1-score"].tolist(),   
+        ["Fake LeNet"] + data_dict["FAKE_LeNet"]["f1-score"].tolist(),   
+        ["REAL VGG16"] + data_dict["REAL_VGG16"]["f1-score"].tolist()], 
+        headers = header_labels, 
+        tablefmt="github"
+    )
+
+    return table
+
+
 
 def main(): 
-    # define paths
+    # define paths 
     path = pathlib.Path(__file__)
-    testpath = path.parents[1] / "test"
-    resultspath = path.parents[1] / "results"
+    resultspath = path.parents[1] / "results" # get raw results 
 
-    filepath = resultspath / "FAKE_LeNet_metrics_11e.txt"
+    savepath = path.parents[1] / "visualisations" # define path for visualisations going in the readme 
+    savepath.mkdir(exist_ok=True)
 
-    data = create_data_from_metric_txt(filepath)
-    print(data["class"])
-    
+    # get dataframes
+    metrics_data = create_metrics_dataframes(resultspath)
+
+    print(create_table(metrics_data))
 
 if __name__ == "__main__":
     main()
--- src/visualise.py
+++ src/visualise.py
@@ -1,7 +1,9 @@
 '''
 Script for self-assigned Assignment 4, Visual Analytics, Cultural Data Science, F2023
 
-Visualise the training histories of two models side by side, thereby creating three plots, one for each model (NN, LeNet, VGG16).
+Script to create plots and table displayed in README. Concretely, the script contains functions which: 
+1. Visualise the training histories of two models side by side, thereby creating three plots, one for each model (NN, LeNet, VGG16).
+2. Create dataframes from metrics .txt files and from those dataframes creates an overview table displaying all F1 scores 
 
 @MinaAlmasi
 '''
@@ -15,24 +17,30 @@ import re
 from matplotlib import pyplot as plt
 import numpy as np
 
-def load_model_histories(path):
+# table 
+from tabulate import tabulate 
+
+def load_model_histories(resultspath):
     '''
     Loads all history objects from a given path and returns them in a dictionary.
 
     Args:
-        - path: path to directory containing history objects
+        - resultspath: path to directory containing history objects
     
     Returns: 
         - history_objects: dictionary containing all history objects in path 
     '''
-
+    # define empty dictionary where history objects will be saved 
     history_objects = {}
 
-    for file in path.iterdir():
-        if "history" in file.name:
-            with open(path / file.name , 'rb') as f:
+    for file in resultspath.iterdir():
+        if "history" in file.name: # open all files which have "history" in their name
+            with open(resultspath / file.name , 'rb') as f:
+                # load history object 
                 history = pickle.load(f)
-                history_name = re.sub("_history.pickle", "", file.name) 
+                # define history object name (e.g., "REAL_LeNet_history.pickle" -> "REAL_LeNet")
+                history_name = re.sub("_history.pickle", "", file.name)
+                # add to history_objects dict ! 
                 history_objects[history_name] = history
     
     return history_objects 
@@ -137,11 +145,88 @@ def plot_histories(history1:dict, history2:dict, row_headers:list, savepath:str,
     # save fig 
     fig.savefig(savepath / filename, dpi=300)
 
-def main(): 
-    path = pathlib.Path(__file__)
+def create_data_from_metrics_txt(filepath):
+    '''
+    Create a dataframe from a text file containing the classification report from sklearn.metrics.classification_report
 
-    resultspath = path.parents[1] / "results"
-    savepath = path.parents[1] / "test"
+    Args:
+        - filepath: path to text file
+
+    Returns: 
+        - data: dataframe containing the classification report 
+    '''
+
+    data = pd.read_csv(filepath)
+
+    # replace macro avg and weighted avg with macro_avg and weighted_avg
+    data.iloc[:,0]= data.iloc[:,0].str.replace(r'(macro|weighted)\savg', r'\1_avg', regex=True)
+
+    # split the columns by whitespace
+    data = data.iloc[:,0].str.split(expand=True)
+
+    # define new column names 
+    new_cols = ['class', 'precision', 'recall', 'f1-score', 'support']
+    data.columns = new_cols
+
+    # identify the row with the accuracy score 
+    is_accuracy = data['class'] == 'accuracy'
+
+    # move the accuracy row values into the precision and recall columns (they are placed incorrectly when the columns are split)
+    data.loc[is_accuracy, ['f1-score', 'support']] = data.loc[is_accuracy, ['precision', 'recall']].values
+
+    # set precision and recall to None for the accuracy row
+    data.loc[is_accuracy, ['precision', 'recall']] = None
+
+    return data
+
+def create_metrics_dataframes(resultspath):
+    '''
+    Loads all history objects from a given path and returns them in a dictionary.
+
+    Args:
+        - resultspath: path to directory containing txt files with metrics from scikit-learn's classification report
+    
+    Returns: 
+        - metrics_dfs: dictionary containing all txt files in path 
+    '''
+
+    # define empty dictionary where dataframes will be saved
+    metrics_dfs = {}
+
+    for file in resultspath.iterdir(): 
+        if "metrics" in file.name: # only work on all files which have "metrics in their name"
+            # create dataframe from txt file 
+            metrics_data = create_data_from_metrics_txt(resultspath/file.name)
+            # define metrics name with regex (e.g., "REAL_LeNet_18e.txt" -> "REAL_LeNet")
+            metrics_name = re.sub("_metrics_\d+e.txt", "", file.name)
+            # add to metrics_dfs dict ! 
+            metrics_dfs[metrics_name] = metrics_data
+
+    return metrics_dfs
+
+def create_table(data_dict, metric:str="f1-score"): 
+    # get header for table   
+    header_labels = data_dict["REAL_VGG16"]["class"].tolist()
+
+    # create table 
+    table = tabulate(
+        [["REAL NN"] + data_dict["REAL_NN"][metric].tolist(),   
+        ["FAKE NN"] + data_dict["FAKE_NN"][metric].tolist(),   
+        ["REAL LeNet"] + data_dict["REAL_LeNet"][metric].tolist(),   
+        ["Fake LeNet"] + data_dict["FAKE_LeNet"][metric].tolist(),   
+        ["REAL VGG16"] + data_dict["REAL_VGG16"][metric].tolist()], 
+        headers = header_labels, 
+        tablefmt="github"
+    )
+
+    return table
+
+def main(): 
+    # define paths 
+    path = pathlib.Path(__file__)
+    resultspath = path.parents[1] / "results" # get raw results 
+
+    savepath = path.parents[1] / "visualisations" # define path for visualisations going in the readme 
     savepath.mkdir(exist_ok=True)
 
     # create history plots for real and fake data
@@ -149,6 +234,7 @@ def main():
     for model in ["NN", "LeNet", "VGG16"]:
         plot_histories(history_objects[f"REAL_{model}"], history_objects[f"FAKE_{model}"], [f"REAL {model}", f"FAKE {model}"], savepath, f"{model}_histories.png")
 
+    
 
 
 
