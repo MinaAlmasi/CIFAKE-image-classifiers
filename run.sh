48d6ac9aa3a80d678610c14b7453add69d2bfb37
MinaAlmasi <minaalmasi00@gmail.com>
2023-05-10 13:34:43 +0200

    add python version, visualise in .sh

--- run.sh
+++ run.sh
@@ -5,21 +5,25 @@ source ./env/bin/activate
 
 # preprocess data 
 echo -e "\n [INFO:] Create metadata ..." # user msg 
-python src/metadata.py
+python3.9 src/metadata.py
 
 # run simple neural network 
 echo -e "\n [INFO:] Running classification pipeline with simple neural network ..." # user msg 
-python src/classify_NN.py -data FAKE
-python src/classify_NN.py -data REAL
+python3.9 src/classify_NN.py -data FAKE
+python3.9 src/classify_NN.py -data REAL
 
 # run LeNet CNN
 echo -e "\n [INFO:] Running classification pipeline with LeNet CNN ..." # user msg 
-python src/classify_LeNet.py -data FAKE
+python3.9 src/classify_LeNet.py -data FAKE
 python src/classify_LeNet.py -data REAL
 
+# run VGG16
 echo -e "\n [INFO:] Running classification pipeline with VGG16 CNN ..." # user msg 
-python src/classify_VGG16.py -data FAKE 
-python src/classify_VGG16.py -data REAL 
+python3.9 src/classify_VGG16.py -data FAKE 
+python3.9 src/classify_VGG16.py -data REAL 
+
+# run visualisation
+python3.9 src/visualise.py 
 
 # deactivate virtual environment
 deactivate
